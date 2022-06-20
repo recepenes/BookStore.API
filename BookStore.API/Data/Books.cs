332f21947ee701019c5310496279cc69e0c57add
recepenes <turktenacious@gmail.com>
2022-06-20 21:05:32 +0300

    Added Entity Framework Core Added Books model

---
+++ BookStore.API/Data/Books.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.API.Data
+{
+    public class Books
+    {
+        [Key]
+        public int BookId { get; set; }
+        public int Title { get; set; }
+        public string Desctription { get; set; }
+
+    }
+}
--- BookStore.API/Program.cs
+++ BookStore.API/Program.cs
@@ -1,8 +1,13 @@
+using BookStore.API.Data;
 using BookStore.API.Repository;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+ConfigurationManager configuration = builder.Configuration;
 // Add services to the container.
+
+builder.Services.AddDbContext<BookStoreContext>(
+    options => options.UseSqlServer(configuration.GetConnectionString("BookStoreDB")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
